03cd7f22d3b2f54dc743a7a67e865ea358e2325d
Tal Rimer <rimer3@walla.co.il>
2021-10-03 14:21:13 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { RouterModule, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
+
+
+constructor(private router: Router) {}
+
+
+ngOnInit() {
+  this.router.navigate([''])
+}
   title = 'smarti-project';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,41 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DogListComponent } from './componenets/dog-list/dog-list.component';
+import { CreateDogFormComponent } from './componenets/create-dog-form/create-dog-form.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import { FiltersSearchComponent } from './componenets/filters-search/filters-search.component';
+import { SearchBarPipe } from './search-bar.pipe';
+
+const routes: Routes = [
+  {path: '', component: DogListComponent},
+  {path:'createDog', component: CreateDogFormComponent},
+  {path:'', component:DogListComponent},
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DogListComponent,
+    CreateDogFormComponent,
+    FiltersSearchComponent,
+    SearchBarPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    ReactiveFormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    MatAutocompleteModule,
+    [RouterModule.forRoot(routes),BrowserAnimationsModule]
   ],
+  exports: [ RouterModule ],
   providers: [],
   bootstrap: [AppComponent]
 })
